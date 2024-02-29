222bd3aea8dc0e7ede0c726432161deed5fa4434
AurelienDesplanque <161575737+AurelienDesplanque@users.noreply.github.com>
2024-02-29 10:55:00 +0100

    add posts

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,9 +1,19 @@
-
-
-
 <template>
     <div>
-
-        Helloooo from index of events
-        </div>
+        <li v-for="post of posts" :key="post.slug">
+            <NuxtLink :to="post.slug">{{ post.title }}</NuxtLink>
+        </li>
+    </div>
 </template>
+  
+<script>
+export default {
+    async asyncData({ $content }) {
+        const posts = await $content("blog").fetch();
+
+        return {
+            posts,
+        };
+    },
+};
+</script>
