4484dc87b483236fd3188eaa798dc79045f67bd8
AurelienDesplanque <161575737+AurelienDesplanque@users.noreply.github.com>
2024-02-29 14:46:31 +0100

    e"é

---
+++ components/Blog.vue
@@ -0,0 +1,22 @@
+
+  
+<script>
+export default {
+    async asyncData({ $content }) {
+        console.log("je rr");
+        const posts = await $content("blog").fetch();
+
+        return {
+            posts,
+        };
+    },
+};
+</script>
+
+<template>
+    <div>
+        <li v-for="post of posts" :key="post.slug">
+            <NuxtLink :to="post.slug">{{ post.title }}</NuxtLink>
+        </li>
+    </div>
+</template>
--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,17 +1,8 @@
 
-  
-<script>
-export default {
-    async asyncData() {
-        console.log("je rr");
-    },
-};
-</script>
-
 <template>
     <div>
-        <li v-for="post of posts" :key="post.slug">
-            <NuxtLink :to="post.slug">{{ post.title }}</NuxtLink>
-        </li>
+        salut lindex
+
+        <Blog />
     </div>
 </template>
