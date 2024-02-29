e7043d033e13e9df79ac800c9ebd53bef06dfda6
AurelienDesplanque <161575737+AurelienDesplanque@users.noreply.github.com>
2024-02-29 13:49:32 +0100

    ddd

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -1,11 +1,7 @@
 <template>
-    <div>fffhello
+    <div>
         <li v-for="post of posts" :key="post.slug">
             <NuxtLink :to="post.slug">{{ post.title }}</NuxtLink>
-        </li>
-
-        <li v-for="post in posts" :key="post.slug">
-            <NuxtLink :to="`/blog/${post.slug}`">{{ post.title }}</NuxtLink>
         </li>
     </div>
 </template>
@@ -13,10 +9,7 @@
 <script>
 export default {
     async asyncData({ $content }) {
-        console.log("RRR");
         const posts = await $content("blog").fetch();
-        console.log(posts); // Vérifiez les données récupérées dans la console
-
 
         return {
             posts,
