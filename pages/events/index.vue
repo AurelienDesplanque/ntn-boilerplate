519b19e537f7c97e1207e1e8aee94d4a9f8f29fd
AurelienDesplanque <161575737+AurelienDesplanque@users.noreply.github.com>
2024-02-29 13:52:19 +0100

    dae

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -8,7 +8,9 @@
   
 <script>
 export default {
+    console.log("je raaaar");
     async asyncData({ $content }) {
+        console.log("je rr");
         const posts = await $content("blog").fetch();
 
         return {
