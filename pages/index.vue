b9726a53fd6512ed7605008de3e48494fc5177e2
AurelienDesplanque <161575737+AurelienDesplanque@users.noreply.github.com>
2024-02-29 14:29:48 +0100

    fic maybe

--- pages/index.vue
+++ pages/index.vue
@@ -7,8 +7,6 @@ const { $hello } = useNuxtApp()
 </script>
 
 <template>
-    <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-    <!-- Check si un token d'invitation existe-->
     <div>
         <h2>About me, iiiindex vue</h2>
         <img src="@/assets/img1.png" />
@@ -22,3 +20,13 @@ h2 {
     font-size: 36px;
 }
 </style>
+
+<script setup>
+  
+useHead({
+  script: [
+    { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
+  ],
+});
+
+</script>
